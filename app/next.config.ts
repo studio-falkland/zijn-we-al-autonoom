6d84080fce6a60fcdb86fb438fb91d572913c132
Lei Nelissen <lei@codified.nl>
2025-03-07 15:09:56 +0100

    feat: migrate to pnpm This is mostly because the monorepo has issues with the various react versions, which NextJS cannot find

--- app/next.config.ts
+++ app/next.config.ts
@@ -1,8 +1,15 @@
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
-  /* config options here */
-  serverExternalPackages: ['typeorm']
+    /* config options here */
+    serverExternalPackages: ['typeorm'],
+    // output: 'export',
+    experimental: {
+        serverMinification: false,
+    },
+    typescript: {
+        ignoreBuildErrors: true
+    }
 };
 
 export default nextConfig;
