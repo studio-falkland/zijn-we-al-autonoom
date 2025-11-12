ad916201f16105573e0f8d74e1a3212c88762660
Lei Nelissen <lei@codified.nl>
2025-11-12 15:49:44 +0100

    feat: add sector an numer of organisations to ratiocard

--- app/src/components/RatioCard/index.tsx
+++ app/src/components/RatioCard/index.tsx
@@ -1,30 +1,33 @@
-import { Category, DestinationDataset, Region } from '@are-we-dependent/data';
+import { Category, DestinationDataset, Region, Sectors } from '@are-we-dependent/data';
 import { ArrowRight } from 'lucide-react';
 import { getFrequencySet } from './Treemap/queries';
 import Treemap from './Treemap';
 import Concentration from './Concentration';
 import Link from 'next/link';
-import { getCategoryLabel, getDatasetLabel, getRegionLabel } from '@/lib/labels';
+import { getCategoryLabel, getDatasetLabel, getRegionLabel, getSectorLabel } from '@/lib/labels';
 import { getIconForCategory, getIconForDataset, getIconForRegion } from '@/lib/icons';
 
 export interface RatioCardProps {
     region: Region,
     category: Category,
     dataset: DestinationDataset,
+    sector?: Sectors,
 }
 
 export default async function RatioCard({
-    region, category, dataset,
+    region, category, dataset, sector,
 }: RatioCardProps) {
-    const frequencies = await getFrequencySet({ region, category, type: dataset });
+    const frequencies = await getFrequencySet({ region, category, type: dataset, sector });
 
     const RegionIcon = getIconForRegion(region);
     const CategoryIcon = getIconForCategory(category);
     const DatasetIcon = getIconForDataset(dataset);
+
+    const sum = frequencies.reduce((sum, f) => sum + f.frequency, 0);
     
     return (
         <Link
-            className="bg-white p-4 rounded-xl border border-blue-800 box-shadow-zwaa hover:-translate-y-1 transition-transform"
+            className="bg-white p-4 rounded-xl border border-blue-800 box-shadow-zwaa hover:-translate-y-1 transition-transform w-full"
             href={`/${dataset}/${category}`}
         >
             <div className="flex justify-between">
@@ -38,10 +41,22 @@ export default async function RatioCard({
                         <CategoryIcon className="w-3 h-3" />
                         {getCategoryLabel(category)}
                     </div>
+                    {sector && (
+                        <>
+                            <ArrowRight className="w-4" />
+                            <div className="flex items-center gap-2">
+                                <CategoryIcon className="w-3 h-3" />
+                                {getSectorLabel(sector)}
+                            </div>
+                        </>
+                    )}
                     <ArrowRight className="w-4" />
                     <div className="flex items-center gap-2">
                         <DatasetIcon className="w-3 h-3" />
                         {getDatasetLabel(dataset)}
+                    </div>
+                    <div className="text-blue-200">
+                        ({sum} organisaties)
                     </div>
                 </div>
                 <Concentration frequencies={frequencies} />
